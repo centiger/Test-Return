b196e7281d39d046e081db375f0880ebc9d65a9e
centiger <centiger2@gmail.com>
2026-06-11 10:57:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cen-return-exile-matrix-v6-final-fix-linkfix-20260530-jesuslink2-singlebtn-final-return95';
+const CACHE='cen-return-standard-hub-v1-20260611';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
